883653ce6ac2932a79bc16c1eee9a81830b4cab7
Vlad <vladik11012006@gmail.com>
2024-11-19 22:31:44 +0100

    movement done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent (typeof (CharacterController), typeof(Collider))]
 public class PlayerMovement : MonoBehaviour
 {
     public enum PlayerState
@@ -12,39 +13,69 @@ public class PlayerMovement : MonoBehaviour
         standing,
         inAir
     }
-    public PlayerState State { get; private set; }
+    public PlayerState State;
 
     [Header("Movement")]
     [SerializeField] private float walkSpeed;
     [SerializeField] private float runSpeed;
-    public float currentMoveSpeed {  get; private set; }
+    public float currentMoveSpeed;
     public float speedModifier;
 
+    [SerializeField] private CharacterController controller;
     [Header("Jump")]
-    [SerializeField] private float jumpPower;
-    public bool isGrounded;
+    [SerializeField] private float gravity;
+    [SerializeField] private float jumpHeight;
+    [Space(10)]
+    [SerializeField] private float groundDistance;
+    [SerializeField] private LayerMask groundMask;
+    [Space(10)]
+    private Vector3 velocity;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private Transform groundCheck;
+
+    [Header("Camera")]
+    [SerializeField] private float sensitivity;
+    [SerializeField] private Camera mainCam;
+    private float camX = 0f;
 
     void Start()
     {
         speedModifier = 1f;
+        controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    private float horizontal;
+    private float vertical;
+    private bool isRunning;
+
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        isRunning = Input.GetButton("Run");
+        RotateCam();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         StateManage();
         SpeedManage();
+        MoveCharacter();
     }
 
     private void StateManage() //assigns player state
     {
         if(!isGrounded) State = PlayerState.inAir;
 
-        else if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Horizontal") != 0) // if any movement button is pressed
+        else if(horizontal != 0 || vertical != 0) // if any movement button is pressed
         {
-            if(Input.GetButton("Run")) State = PlayerState.running; //if run button is pressed
-
-            else State = PlayerState.walking; //if run button is not pressed
+            State = isRunning ? PlayerState.running : PlayerState.walking;
         }
 
         else State = PlayerState.standing; //if no button is pressed
@@ -61,6 +92,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void MoveCharacter()
     {
+        Vector3 move = gameObject.transform.right * horizontal + gameObject.transform.up * vertical;
+        controller.Move(currentMoveSpeed * Time.deltaTime * move);
 
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime * 3;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    private void RotateCam()
+    {
+        float turner = Input.GetAxis("Mouse X") * sensitivity;
+        float looker = Input.GetAxis("Mouse Y") * sensitivity;
+
+        if (turner != 0)
+        {
+            transform.eulerAngles += new Vector3(0, turner, 0);
+        }
+        if (looker != 0)
+        {
+            camX -= looker;
+            camX = Mathf.Clamp(camX, -140f, -50f);
+
+            mainCam.transform.localRotation = Quaternion.Euler(camX, 0f, 0f);
+        }
     }
 }
+
